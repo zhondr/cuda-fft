08064ceb54ee1e509080fb99dcc8c628687193ac
zhondr <zhandarbek@gmail.com>
2020-03-26 16:40:18 +0600

    only fft time calculated

--- plot.py
+++ plot.py
@@ -1,24 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_file1 = np.loadtxt('CpuTimeData_total.txt',delimiter='	')
-data_file2 = np.loadtxt('GpuTimeData_total.txt',delimiter='	')
+data_file1 = np.loadtxt('CpuTimeData_onlyfft.txt',delimiter='	')
+data_file2 = np.loadtxt('GpuTimeData_onlyfft.txt',delimiter='	')
+data_file3 = np.loadtxt('FpgaTimeData_onlyfft.txt',delimiter='	')
 
+fftsize = data_file1[:,0]
+cputime = data_file1[:,1]
+gputime = data_file2[:,1]
+fpgatime = data_file3[:,1]
 
-matrixsize = data_file1[:,0]
-gpu = data_file2[:,1]
-cpu = data_file1[:,1]
-
-print(matrixsize)
-
-plt.plot(matrixsize,cpu,matrixsize,gpu)
-plt.legend(['CPU','GPU'],loc='best')
-plt.xlabel('r')
-plt.ylabel('time')
+plt.plot(fftsize,cputime,fftsize,gputime,fftsize,fpgatime)
+plt.legend(['CPU','GPU','FPGA'],loc='best')
+plt.xlabel('fftsize')
+plt.ylabel('time(us)')
 plt.show()
 
-plt.semilogy(matrixsize,cpu,matrixsize,gpu)
-plt.legend(['CPU','GPU'],loc='best')
-plt.xlabel('r')
-plt.ylabel('time')
+plt.semilogy(fftsize,cputime,fftsize,gputime,fftsize,fpgatime)
+plt.legend(['CPU','GPU','FPGA'],loc='best')
+plt.xlabel('fftsize')
+plt.ylabel('logtime')
 plt.show()
